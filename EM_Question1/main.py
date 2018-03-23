4d4d55973e55fa72b1ea64a61e058b7dc0a11494
Dominic Cupo <Dominic.Cupo@gmail.com>
2018-03-23 14:42:16 -0400

    Added flag and handling for showing cluster plots

--- EM_Question1/main.py
+++ EM_Question1/main.py
@@ -6,14 +6,18 @@ import EM
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('EM Calculations')
+
     parser.add_argument('data_file', type=str, help='The input file to read the EM points from')
     parser.add_argument('num_clusters', help='The number of Clusters to generate')
+
+    parser.add_argument('-p', '--plot', action='store_true', help='Flag for showing generated scatterplot')
 
     args = vars(parser.parse_args())
 
     data = genfromtxt(args['data_file'], delimiter=',').T
+    show_plot = args['plot']
 
-    em = EM.ExpectationMaximization(data)
+    em = EM.ExpectationMaximization(data, show_plot)
 
     if args['num_clusters'] == 'X':
         # do EM Question 2
