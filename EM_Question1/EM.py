1e5f39021fbd82f96e15792244d55e3b52cc663b
Aayush <aayushjain27894@gmail.com>
2018-03-23 13:55:33 -0400

    Small change of the norm_pdf_multivariate()

--- EM_Question1/EM.py
+++ EM_Question1/EM.py
@@ -18,12 +18,6 @@ class ExpectationMaximization:
 
         self._prob = 0
 
-    def normalize(self, prob):
-        for column in range(self._num_data_points):
-            norm = sum(prob[:, column])
-            prob[:, column] /= norm
-        return prob
-
     def calculate_clusters(self, num_clusters=0):
         if num_clusters is not 0:
             self._prob = self.normalize(np.random.rand(num_clusters, self._num_data_points))
@@ -32,24 +26,32 @@ class ExpectationMaximization:
 
             while not shouldExit:
                 pass
+    def normalize(self, prob):
+        for column in range(self._num_data_points):
+            norm = sum(prob[:, column])
+            prob[:, column] /= norm
+        return prob
 
-    def norm_pdf_multivariate(self, k, j, data, mu, sigma):
+    def norm_pdf_multivariate(self, data, mu, sigma):
         size = len(self._data)
-        if size == len(mu) and (size, size) == sigma[k].shape:
-            det = np.linalg.det(sigma[k])
-            if det == 0:
-                raise NameError("The covariance matrix can't be singular")
+        for i in range(self._num_clusters):
+            for j in range(self._num_data_points):
+                if size == len(mu) and (size, size) == sigma[i].shape:
+                    det = np.linalg.det(sigma[i])
+                    if det == 0:
+                        raise NameError("The covariance matrix can't be singular")
 
-            norm_const = 1.0/(np.power((2*pi), float(size)/2) * np.power(det, 1.0/2))
+                    norm_const = 1.0/(np.power((2*pi), float(size)/2) * np.power(det, 1.0/2))
 
-            x_mu = data[:, j] - mu[k]
-            ainv = np.linalg.inv(sigma[k])
-            result = np.power(math.e, -0.5 * (x_mu * ainv * x_mu.T))
-            result = norm_const * result
-            return result
+                    x_mu = data[:, j] - mu[i]
+                    ainv = np.linalg.inv(sigma[i])
+                    result = np.power(math.e, -0.5 * (x_mu.T * ainv * x_mu))
+                    result = norm_const * result
 
-        else:
-            raise NameError("The dimensions of the input don't match")
+                else:
+                    raise NameError("The dimensions of the input don't match")
+        prob[:, j] =
+        return result
 
     def maximization(self):
         pic = np.mean(self._prob, axis=1)
@@ -63,11 +65,7 @@ class ExpectationMaximization:
 
 
     def expectation(self, mu, prob, data, sigma, pic):
-        for a in range(self._num_clusters):
-            for b in range(self._num_data_points):
-                result = norm_pdf_multivariate(a, b, data, mu, sigma)
-                prob[a, b] = result
-
+        result = norm_pdf_multivariate(self., mu, sigma, prob)
         prob = pic * prob
         prob = normalize(prob)
 
