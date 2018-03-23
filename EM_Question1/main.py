eb79128c802063e432fde2be0365c586723dfb19
Dominic Cupo <Dominic.Cupo@gmail.com>
2018-03-22 20:33:12 -0400

    removed metavar for arguments, imported EM properly

--- EM_Question1/main.py
+++ EM_Question1/main.py
@@ -2,14 +2,18 @@ import argparse
 
 from numpy import genfromtxt
 
+import EM
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('EM Calculations')
-    parser.add_argument('data_file', metavar='FILE', type=str, help='The input file to read the EM points from')
-    parser.add_argument('num_clusters', metavar='CLUSTERS', help='The number of Clusters to generate')
+    parser.add_argument('data_file', type=str, help='The input file to read the EM points from')
+    parser.add_argument('num_clusters', help='The number of Clusters to generate')
 
     args = vars(parser.parse_args())
 
     data = genfromtxt(args['data_file'], delimiter=',').T
+
+    em = EM.ExpectationMaximization(data)
 
     if args['num_clusters'] == 'X':
         # do EM Question 2
